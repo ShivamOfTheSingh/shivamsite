15e3c188e1cf99d003c1bc9a9bc39bf1a4ab1edd
Shivam Singh <ssingh@b01-aruba-authenticated-10-110-232-92.wlan.cpp.edu>
2023-10-09 15:27:40 -0700

    Modified Input Feilds

--- src/components/contacts/EmailPrompt.jsx
+++ src/components/contacts/EmailPrompt.jsx
@@ -1,15 +1,23 @@
-import React from 'react'
-import './EmailPrompt.css'
+import React from 'react';
+import './EmailPrompt.css';
 
 const EmailPrompt = () => {
   return (
     <div className='EmailMain'>
-      <p>Email Me!</p>
-          <input className='ReturnEmail' />
-          <input className='Subject' />
-          <input className='MailContents' />
+      <p className='Title'>Email Me!</p>
+      <form className='InputForm'>
+        <label className='InputField'>
+          <input className='ReturnEmail' placeholder="Return Email" />
+        </label>
+        <label className='InputField'>
+          <input className='Subject' placeholder="Subject" />
+        </label>
+        <label className='InputField'>
+          <textarea className='MailContents' placeholder="Message" rows="4" cols="50" />
+        </label>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default EmailPrompt
+export default EmailPrompt;
