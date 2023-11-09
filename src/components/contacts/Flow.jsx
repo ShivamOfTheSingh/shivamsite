5019474af17bf1de3d84cef6613f87ea104fa6a2
Shivam Singh <shivam2003.singh@gmail.com>
2023-11-08 22:07:40 -0800

    Intereative mouse movement perlin noise feild

--- src/components/contacts/Flow.jsx
+++ src/components/contacts/Flow.jsx
@@ -1,67 +1,67 @@
-// A Perlin Noise Flow Feild
-import 'react'
+import React from 'react';
 import { ReactP5Wrapper } from "@p5-wrapper/react";
-import "../../App.css"
+import "../../App.css";
 
 const Flow = () => {
     let w, h, nums;
     let zOff = 0;
     let scl = 0.0005;
-    let speed = 11;
+    let speed = 10;
     let particles = [];
     let cursor;
 
     const setup = (p5, canvasParentRef) => {
-        // Sets initial conditions
-        w = p5.windowWidth
-        h = p5.windowHeight
-        nums = 2000
-        p5.frameRate(30)
-        p5.createCanvas(w, h, p5.WEBGL).parent(canvasParentRef)
+        w = p5.windowWidth;
+        h = p5.windowHeight;
+        nums = 2000;
+        p5.frameRate(30);
+        p5.createCanvas(w, h, p5.WEBGL).parent(canvasParentRef);
 
-        // Creates particle vectors
         for (let i = 0; i < nums; i++) {
-            particles.push(p5.createVector(p5.random(w), p5.random(h)))
+            particles.push(p5.createVector(p5.random(w), p5.random(h)));
         }
 
         cursor = p5.createVector(p5.mouseX, p5.mouseY);
     }
 
     const draw = (p5) => {
-        // Styling
         p5.background(getComputedStyle(document.documentElement).getPropertyValue('--main-bg-color'));
         p5.stroke(getComputedStyle(document.documentElement).getPropertyValue('--main-text-color'));
-        p5.translate(-w / 2, -h / 2)
-        p5.strokeWeight(3.5)
+        p5.translate(-w / 2, -h / 2);
+        p5.strokeWeight(3.5);
         cursor.set(p5.mouseX, p5.mouseY);
 
-        // Brains of the flow feild, takes the positions and modifies the particles positions given by noise.
         for (let i = 0; i < nums; i++) {
-            let p = particles[i]
-            p5.point(p.x, p.y)
-            let n = p5.noise(p.x * scl, p.y * scl, zOff * 1.5);
-            let angle = p5.TWO_PI * n
-            p.x += Math.cos(angle) * speed
-            p.y += Math.sin(angle) * speed
+            let p = particles[i];
+            let distance = p5.dist(p.x, p.y, cursor.x, cursor.y);
 
-            let dir = p5.createVector(p.x, p.y);
-            dir.sub(cursor);
-            dir.normalize();
-            let repulsion = 6;
-            dir.mult(repulsion);
-            p.add(dir);
-
-            if (!onScreen(p)) {
-                p.x = p5.random(p5.windowWidth)
-                p.y = p5.random(p5.windowHeight)
+            if (distance < 50) {
+                let dir = p5.createVector(p.x, p.y);
+                dir.sub(cursor);
+                dir.normalize();
+                let repulsion = 7;
+                dir.mult(repulsion);
+                p.add(dir);
             }
+
+            let n = p5.noise(p.x * scl, p.y * scl, zOff * 3);
+            let angle = p5.TWO_PI * n;
+            p.x += Math.cos(angle) * speed;
+            p.y += Math.sin(angle) * speed;
+
+            if (!onScreen(p, w, h)) {
+                p.x = p5.random(p5.windowWidth);
+                p.y = p5.random(p5.windowHeight);
+            }
+
+            p5.point(p.x, p.y);
         }
 
-        zOff += 0.002
+        zOff += 0.002;
     }
 
-    const onScreen = (v) => {
-        return v.x >= 0 && v.x <= w && v.y >= 0 && v.y < h
+    const onScreen = (v, w, h) => {
+        return v.x >= 0 && v.x <= w && v.y >= 0 && v.y <= h;
     }
 
     const windowResized = (p5) => {
@@ -80,4 +80,4 @@ const Flow = () => {
     )
 }
 
-export default Flow
+export default Flow;
