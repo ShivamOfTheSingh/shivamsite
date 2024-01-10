4c0526f5d8cacb823634e69d5ec16a234f93c7b1
Shivam Singh <shivam2003.singh@gmail.com>
2024-01-10 14:06:17 -0800

    Added 3d models, 3d model rotation+interativitiy, and added stylistic elements to About Me page

--- src/components/aboutme/AboutMe.jsx
+++ src/components/aboutme/AboutMe.jsx
@@ -25,9 +25,11 @@ const AboutMe = (props) => {
                 	<div className='Facts'>
                       <p onClick={() => changeModel(1)}>And I am a student at California Polytechnic University, Pomona, pursuing a degree in Computer Science.</p>
                       <p onClick={() => changeModel(2)}>And I am a dog owner (his name is Champ).</p>
+                      <p onClick={() => changeModel(2.5)}>And I am located in the Bay Area.</p>
                       <p onClick={() => changeModel(3)}>And I am passionate about data science, machine learning, and cloud computing, and I'm actively seeking an internship in these fields.</p>
                       <p onClick={() => changeModel(4)}>And I am a cooking enthusiast and an enjoyer of physical activity.</p>
                       <p onClick={() => changeModel(5)}>And I am proficient in Python, C/C++, Java, JavaScript, and more. Experienced with AWS, Google Cloud, Keras/TensorFlow, and more.</p>
+                      <p onClick={() => changeModel(2)}>And I am a lover of samosas.</p>
                       <p onClick={() => changeModel(6)}>And, most of all, I am a nerd.</p>
                   </div>
                 </div>
--- src/components/aboutme/Model.jsx
+++ src/components/aboutme/Model.jsx
@@ -1,31 +1,30 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Canvas } from '@react-three/fiber';
 import './Model.css';
 import { OrbitControls } from '@react-three/drei';
 import { Dumbell } from './Models/Dumbell';
 import { Nerd } from './Models/Nerd';
 import { Cloud } from './Models/Cloud';
-import { Cpp } from './Models/Cpp';
 import { Python } from './Models/Python';
 
 const Model = ({ modelName }) => {
   let currModel;
 
   switch (modelName) {
-    case 1:
-      currModel = <Cpp />;
+    case 3:
+      currModel = <Cloud />
       break;
     case 4:
       currModel = <Dumbell />;
       break;
     case 5:
-      currModel = <Cloud />;
+      currModel = <Python />;
       break;
     case 6:
       currModel = <Nerd />;
       break;
     default:
-      currModel = <Python />
+      currModel = <Python />;
   }
 
   return (
@@ -34,6 +33,13 @@ const Model = ({ modelName }) => {
         <Canvas className='canvas' size={[1200, 1200]}>
           <OrbitControls />
           <ambientLight intensity={1} />
+
+          {/* Add a point at (0, 0, 0) */}
+          <mesh position={[0, 0, 0]}>
+            <sphereGeometry args={[0.1, 32, 32]} />
+            <meshStandardMaterial color='red' />
+          </mesh>
+
           {currModel}
         </Canvas>
       </div>
