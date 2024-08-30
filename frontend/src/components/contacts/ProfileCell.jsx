779f080f34761353c7a6fc8b8b58121bec91ae2f
ShivamOfTheSingh <shivam2003.singh@gmail.com>
2024-08-29 18:06:36 -0700

    linkedin api sucks, i did what i could

--- frontend/src/components/contacts/ProfileCell.jsx
+++ frontend/src/components/contacts/ProfileCell.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import "./Cell.css";
 
-const ProfileCell = ({ imagelink, username, cat }) => {
+const ProfileCell = ({ imagelink, username, cat, url }) => {
   return (
-    <div className='CellMain'>
+    <div className='CellMain' title={`My ${cat} profile - ${username}`}>
       <img className='pfp' src={imagelink} alt="Profile" />
       <div className='text'>
         <h1>Come check out my {cat}!</h1>
-        <a href={username} target="_blank" rel="noopener noreferrer">{username} &#8594;</a>
+        <a href={url} target="_blank" rel="noopener noreferrer">{username} &#8594;</a>
       </div>
     </div>
   );
--- frontend/src/components/contacts/Profiles.jsx
+++ frontend/src/components/contacts/Profiles.jsx
@@ -18,22 +18,27 @@ const Profiles = () => {
     }
 
     const fetLinkedIn = async () => {
-      
+      try {
+        const user = await axios.get('http://localhost:6969/linkedin/user')
+        setLinked(user.data)
+        console.log(user.data)
+      } catch (error) {
+        console.error(error)
+      }
     }
 
     fetchGitData()
+    fetLinkedIn()
   }, [])
 
   return (
     <div className='ProfileMain'>
-      {gituserinfo ? (
+      {gituserinfo && linkedinuserinfo && (
         <>
-          <ProfileCell imagelink={gituserinfo.avatar_url} username={gituserinfo.login} cat={"GitHub"}/>
-          <ProfileCell />
+          <ProfileCell imagelink={gituserinfo.avatar_url} username={gituserinfo.login} cat={"GitHub"} url={gituserinfo.html_url}/>
+          <ProfileCell imagelink={linkedinuserinfo.picture} username={linkedinuserinfo.name} cat={"LinkedIn"} url={"https://www.linkedin.com/in/shivamofthesingh/"}/>
           <ProfileCell />
         </>
-      ) : (
-        <p>Loading...</p>
       )}
     </div>
   );
