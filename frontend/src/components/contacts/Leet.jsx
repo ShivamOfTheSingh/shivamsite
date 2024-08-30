4d38fd61e9384d14ffcbb68b8d976789e2f675f8
ShivamOfTheSingh <shivam2003.singh@gmail.com>
2024-08-30 03:01:28 -0700

    added some leet stats

--- frontend/src/components/contacts/Leet.jsx
+++ frontend/src/components/contacts/Leet.jsx
@@ -5,7 +5,7 @@ import "./leet.css"
 const Leet = () => {
     const [leetdata, setLeet] = useState(null)
     useEffect(() => {
-        const fetchleet = async() => {
+        const fetchleet = async () => {
             try {
                 const response = await axios.get("http://localhost:6969/leet/user")
                 setLeet(response.data)
@@ -20,25 +20,30 @@ const Leet = () => {
 
     return (
         <>
-        {
-            leetdata &&
-            <a className="leetmain" title={"My Leetcode Profile"} href={"https://leetcode.com/u/Shivam_Of_The_Singh/"} target="_blank" rel="noopener noreferrer">
-                <div>My Leetcode Profile</div>
-                <div className="bottomside">
-                    <div className="leftside">
-                        <div className="usernom">{leetdata.matchedUser.username}</div>
-                        <img src={leetdata.matchedUser.profile.userAvatar} />
-                        <div>Rank: {leetdata.matchedUser.profile.ranking}</div>
+            {
+                leetdata &&
+                <a className="leetmain" title={"My Leetcode Profile"} href={"https://leetcode.com/u/Shivam_Of_The_Singh/"} target="_blank" rel="noopener noreferrer">
+                    <div>My Leetcode Profile</div>
+                    <div className="bottomside">
+                        <div className="leftside">
+                            <img src={leetdata.matchedUser.profile.userAvatar} />
+                            <div className="usernom">{leetdata.matchedUser.username}</div>
+                            <div>Rank: {leetdata.matchedUser.profile.ranking}</div>
+                        </div>
+                        <div className="middleside">
+                            lol
+                        </div>
+                        <div className="rightside">
+                            <ul>
+                                <li>Total: {leetdata.matchedUser.submitStats.acSubmissionNum[0].count}</li>
+                                <li>Easy: {leetdata.matchedUser.submitStats.acSubmissionNum[1].count}</li>
+                                <li>Medium: {leetdata.matchedUser.submitStats.acSubmissionNum[2].count}</li>
+                                <li>Hard: {leetdata.matchedUser.submitStats.acSubmissionNum[3].count}</li>
+                            </ul>
+                        </div>
                     </div>
-                    <div className="middleside">
-                        lol
-                    </div>
-                    <div className="rightside">
-                        rightside
-                    </div>
-                </div>
-            </a>
-        }
+                </a>
+            }
         </>
     )
 }
