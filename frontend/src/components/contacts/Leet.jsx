f083ae3892ca4325a6f11a86ecd922f40d298d85
ShivamOfTheSingh <shivam2003.singh@gmail.com>
2024-09-03 15:46:03 -0700

    lolol

--- frontend/src/components/contacts/Leet.jsx
+++ frontend/src/components/contacts/Leet.jsx
@@ -37,12 +37,30 @@ const Leet = () => {
                             <div>Rank: {leetdata.matchedUser.profile.ranking}</div>
                         </div>
                         <div className="middleside">
-                            <ul>
-                                <li>Total: {leetdata.matchedUser.submitStats.acSubmissionNum[0].count}</li>
-                                <li>Easy: {leetdata.matchedUser.submitStats.acSubmissionNum[1].count}</li>
-                                <li>Medium: {leetdata.matchedUser.submitStats.acSubmissionNum[2].count}</li>
-                                <li>Hard: {leetdata.matchedUser.submitStats.acSubmissionNum[3].count}</li>
-                            </ul>
+                            <div>I have completed a total of <div className="num">{leetdata.matchedUser.submitStats.acSubmissionNum[0].count} problems</div></div>
+                            <div className="barchart">
+                                <div className="bar">
+                                    <div className="bardata" style={{
+                                        width: `${(leetdata.matchedUser.submitStats.acSubmissionNum[1].count / leetdata.allQuestionsCount[1].count) * 100}%`,
+                                        backgroundColor: "green"
+                                    }}>Easy</div>
+                                    <div className="frac">{leetdata.matchedUser.submitStats.acSubmissionNum[1].count} / {leetdata.allQuestionsCount[1].count}</div>
+                                </div>
+                                <div className="bar">
+                                    <div className="bardata" style={{
+                                        width: `${(leetdata.matchedUser.submitStats.acSubmissionNum[2].count / leetdata.allQuestionsCount[2].count) * 100}%`,
+                                        backgroundColor: "gold"
+                                    }}>Medium</div>
+                                    <div className="frac">{leetdata.matchedUser.submitStats.acSubmissionNum[2].count} / {leetdata.allQuestionsCount[2].count}</div>
+                                </div>
+                                <div className="bar">
+                                    <div className="bardata" style={{
+                                        width: `${(leetdata.matchedUser.submitStats.acSubmissionNum[3].count / leetdata.allQuestionsCount[3].count) * 100}%`,
+                                        backgroundColor: "crimson"
+                                    }}>Hard</div>
+                                    <div className="frac">{leetdata.matchedUser.submitStats.acSubmissionNum[3].count} / {leetdata.allQuestionsCount[3].count}</div>
+                                </div>
+                            </div>
                         </div>
                         <div className="rightside">
                             <div className="capt">
