3fcdf07506cda2bbf79a4c9b123cd9e241f91c26
ShivamOfTheSingh <shivam2003.singh@gmail.com>
2024-09-22 22:25:14 -0700

    fixed spacign issues

--- frontend/src/components/projects/History.jsx
+++ frontend/src/components/projects/History.jsx
@@ -286,24 +286,26 @@ const History = ({ repo }) => {
     <div className='histmain'>
       <div className='hist_head'>
         <p>My Github Activity For The </p>
-        <button
-          className={selected === 'week' ? 'selected' : ''}
-          onClick={() => setSelected('week')}
-        >
-          Week
-        </button>
-        <button
-          className={selected === 'year' ? 'selected' : ''}
-          onClick={() => setSelected('year')}
-        >
-          Year
-        </button>
-        <button
-          className={selected === 'start' ? 'selected' : ''}
-          onClick={() => setSelected('start')}
-        >
-          Start of Time
-        </button>
+        <div>
+          <button
+            className={selected === 'week' ? 'selected' : ''}
+            onClick={() => setSelected('week')}
+          >
+            Week
+          </button>
+          <button
+            className={selected === 'year' ? 'selected' : ''}
+            onClick={() => setSelected('year')}
+          >
+            Year
+          </button>
+          <button
+            className={selected === 'start' ? 'selected' : ''}
+            onClick={() => setSelected('start')}
+          >
+            Start of Time
+          </button>
+        </div>
       </div>
       <div className='stats'>{getCurrentStats()}</div>
     </div>
