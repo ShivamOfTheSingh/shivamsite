644e9f240734e4964eb5894057fcbacfbf8f98c1
ShivamOfTheSingh <shivam2003.singh@gmail.com>
2024-09-29 17:37:15 -0700

    finished prod backend

--- frontend/src/components/contacts/index.js
+++
@@ -1,3 +0,0 @@
-export { default as Flow } from './Flow'
-export { default as EmailPrompt } from './EmailPrompt'
-export { default as Profiles } from './Profiles'
--- frontend/src/components/projects/SingleRepo.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const SingleRepo = () => {
-  return (
-    <div>SingleRepo</div>
-  )
-}
-
-export default SingleRepo
